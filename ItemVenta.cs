5cf823575eb94680b7bf73bb874b16f861bccc93
leonardo Gomez <leonardo.gomez@ucrso.info>
2021-12-01 19:36:57 -0600

    Creacion de Clases en rama 1

---
+++ ItemVenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ItemVenta
+{
+	private int itemventaID { set; get; };
+	private int cantidadV { set; get; };
+	private float precioU { set; get; };
+	private Venta numVenta { set; get; };
+	private Producto producto { set; get; };
+
+	public ItemVenta(int id, int cant,float pre,Venta venta, Producto producto)
+	{
+		itemventaID = id;
+		cantidadV = cant;
+		precioU = pre;
+		numVenta = venta;
+		producto = producto;
+	}
+
+
+	public ItemVenta()
+	{
+
+	}
+}
---
+++ Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Producto
+{
+	private int productoID { set; get; };
+	private String descrepcion { set; get; };
+	private String preparacion { set; get; };
+
+	public Producto(int id,string desc,string prepa) 
+	{
+		productoID = prepa;
+		descrepcion = desc;
+		preparacion = prepa;
+	}
+
+	public Producto()
+	{
+	}
+}
